cffa3af1be3c1459372cd12ffb180ac74510a1d3
Brett Kulp <103147007+BrettKulp@users.noreply.github.com>
2023-02-05 15:04:58 -0600

    made it so the rom data is only read if the user clicks open in the file chooser and the file chooser goes away if the user clicks cancel

--- ReadGBFC.java
+++ ReadGBFC.java
@@ -2,7 +2,6 @@ import java.io.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-//this class reads the Game Boy game rom file as binary data
 
 public class ReadGBFC{
     JFrame frame;
@@ -11,74 +10,61 @@ public class ReadGBFC{
     JMenuItem m1;
     File romFile;
     JFileChooser fc;
+    byte[] romData;
+
     public ReadGBFC(){
-        
         frame = new JFrame("GameBoy");
         FCListener fcl = new FCListener();
         mb = new JMenuBar();
         m = new JMenu("File");
         fc = new JFileChooser();
-        
-        
+
         m1 = new JMenuItem("Load ROM");
-        
         m.add(m1);
         m1.addActionListener(fcl);
-        mb.add(m); 
-        
+        mb.add(m);
         frame.setJMenuBar(mb);
-        
         frame.setSize(500,500);
         frame.setVisible(true);
-        //File romFile = new File("/Users/brettkulp/Desktop/Capstone/Emulator/src/Tetris (World) (Rev A).gb");
-        
-        //romFile = new File("dmg_boot.bin");
-        
-        
-        
-        
-        
-        
     }
-    public class FCListener implements ActionListener  {
+
+    public class FCListener implements ActionListener {
         public void actionPerformed(ActionEvent e)
         {
             if(e.getSource() == m1)
             {
-                int ret = -1;
-                while(ret != JFileChooser.APPROVE_OPTION)
-                {
-                    ret = fc.showOpenDialog(frame);
-                }
+                int ret = fc.showOpenDialog(frame);
                 if(ret == JFileChooser.APPROVE_OPTION)
                 {
                     romFile = fc.getSelectedFile();
+                    try {
+                        FileInputStream romStream = new FileInputStream(romFile);
+                        romData = new byte[(int)romFile.length()];
+                        romStream.read(romData);
+                        romStream.close();
+                    } catch (IOException ioe) {
+                        ioe.printStackTrace();
+                    }
+                    // call function to print the rom data
+                    printROMData();
                 }
-                byte[] romData = new byte[(int)romFile.length()];
-                try
-                {
-                    FileInputStream romStream = new FileInputStream(romFile);
-                    romStream.read(romData);
-                    romStream.close();
-                }
-                catch (IOException ioe)
-                {
-                    ioe.printStackTrace();
-                }
-        //here is where we do something with the ROM data currently just printing it out in here
-        int i = 0;
-        for (byte b : romData) {
-            if(i  % 10 == 0 && i != 0){
+            }
+        }
+        // we can pass the array with the rom data like this
+        // methodName(romData)
+    }
+
+    public void printROMData() {
+        for (int i = 0; i < romData.length; i++) {
+            if (i % 10 == 0 && i != 0) {
                 System.out.println();
             }
-            System.out.print(String.format("%02X ", b & 0xFF));
-            i++;
-            }
-            }
+            System.out.print(String.format("%02X ", romData[i] & 0xFF));
+            //System.out.print(romData[i]);
         }
     }
-        
-        public static void main(String[] args){
-            new ReadGBFC();
-        }
+
+    public static void main(String[] args){
+        new ReadGBFC();
+    }
 }
