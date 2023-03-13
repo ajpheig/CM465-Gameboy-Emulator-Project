8099060674ad98fd68606d7d7717c952ea2c71af
Alex Pheigaru <111469517+ajpheig@users.noreply.github.com>
2023-03-13 16:03:38 -0500

    Add files via upload
    filled out some Display methods

--- GPU/Display.java
+++ GPU/Display.java
@@ -1,7 +1,7 @@
 package GPU;
 
-
 import javax.swing.*;
+import java.awt.image.BufferedImage;
 
 public class Display {
     // array of all the pixels in the screen
@@ -9,19 +9,24 @@ public class Display {
     private int screenWidth;
     private int screenHeight;
     private JFrame frame;
-
+    BufferedImage image = new BufferedImage(160, 144, BufferedImage.TYPE_3BYTE_BGR);;
 
     public Display(int screenWidth, int screenHeight) {
-
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+        screenBuffer = new int[screenHeight * screenWidth];
     }
 
     // update the display with new pixels passed in
     public void render() {
-
+        // draw image to frame, dont know if setRGB updates frame immediately
+        // could loop through screeBuffer and image.setRGB here for every pixel
     }
 
     // set the color of specific pixel passed in at (x,y) to the color passed in
     public void setPixel(int x, int y, int color) {
-
+        screenBuffer[x % 160 + y * 160] = color;
+        // or do you want to setRGB
+        image.setRGB(x, y, color);
     }
 }
