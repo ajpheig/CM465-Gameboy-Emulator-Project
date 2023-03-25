7cf83eb4d3c88008e96318209c676a64e0f8dd70
Brett Kulp <103147007+BrettKulp@users.noreply.github.com>
2023-03-25 17:59:09 -0500

    Update VRAM read to render sprites

--- GPU/PPU.java
+++ GPU/PPU.java
@@ -81,7 +81,8 @@ public class PPU {
         cpu.setPPU(this);
         lcdc.setLCDDisplay(true);
         // printRAM();
-
+        // getSpritesData();
+        //initOAM();
         /*
          * STAT (LCD Status): This register provides information about the current LCD
          * state and controls behavior of LCD interrupt
@@ -96,9 +97,7 @@ public class PPU {
     }
     // called every cycle from the CPU before an opcode is ran to cycle through modes. Every cycle counts as one tick.
     public void updateModeAndCycles() {
-        //getSpritesData();
-        //oam.printSpriteData();
-        List<Integer> spriteIndexesOnLine = new ArrayList<>();
+
         if(modeTicks==1)
             ly.setLY((byte)curY);
         switch (mode) {
@@ -106,12 +105,10 @@ public class PPU {
                 //tileSet=vram.getTileSet();//readies tile set for PPU
                 // get sprites if it is the first tick of OAM read otherwise
                 // move to VRAM READ if it is not the first cycle in OAM read
-                // clear sprites from last scanline
-                spriteIndexesOnLine.clear();
                 if (modeTicks ==1) {//changed to 1 because it is modeTicks is ++ after setting it to zero
                     boolean useTileSet0 = lcdc.getBit(4);
                     boolean useBackgroundMap0 = ((lcdc.getByte())&0b1000)==1;
-                    //load tile map
+                    // load tile map
                     this.loadMap(useTileSet0, useBackgroundMap0);
                     //System.out.println(memory.readByte(0x8027));
                     tileSet=vram.getTileSet();//readies tile set for PPU
@@ -121,7 +118,7 @@ public class PPU {
                     // get data of sprites on screen from mem/ perform dma transfer unofficially
                     initOAM();
                     // determine which sprites are on this line
-                   // System.out.println("curY " + curY);
+                    // System.out.println("curY " + curY);
                     // read the sprite size from the lcdc
                     int spriteSize = memory.readByte(0xFF40) & 0b100;
                     int spriteHeight = spriteSize == 0 ? 8 : 16;
@@ -155,9 +152,6 @@ public class PPU {
                     stat.setMF1(true);
                     stat.setMF2(true);
                     memory.writeByte(0xff0f,0b10);//set IF LCD flag
-
-                    // update STAT register
-                    //stat.setMF3(true);---What this do?-------------------------
                 }
                 break;
             case 3: // VRAM read also known as pixel transfer mode
@@ -176,81 +170,47 @@ public class PPU {
                 //System.out.println("curx:"+curX+" cury:"+curY+" xPos:"+xPos+" yPos:"+yPos+"tileIndex:"+Integer.toHexString(bgTileIndex));
                 display.setPixel(curX, curY, backgroundColor);
                 curX++;
-                // loop through the sprites on this line and display them if they overlap with the current pixel
 
-//                for (int i = 0; i < spriteIndexesOnLine.size(); i++) {
-//                    if (spriteIndexesOnLine.size() > 10) {
-//                        // Sort the sprite indexes by priority
-//                        spriteIndexesOnLine.sort((a, b) -> {
-//                            boolean aPriority = oam.hasSpritePriority(a);
-//                            boolean bPriority = oam.hasSpritePriority(b);
-//                            if (aPriority && !bPriority) {
-//                                return -1;
-//                            } else if (!aPriority && bPriority) {
-//                                return 1;
-//                            } else {
-//                                return 0;
-//                            }
-//                        });
-//                    }
-//                    // Remove any sprites that are not in the top 10 highest priority
-//                    while (spriteIndexesOnLine.size() > 10) {
-//                        spriteIndexesOnLine.remove(10);
-//                    }
-//                    // offest of the sprite in OAM spriteIndexesOnLine.get(i)
-//                    byte[] spriteData = oam.getSpriteData(spriteIndexesOnLine.get(i));
-//                    int spriteTileIndex = Byte.toUnsignedInt(spriteData[2]);
-//                    int spriteTileAttributes = spriteData[3];
-//                    int spritePalette = (spriteTileAttributes >> 4) & 0x1;
-//                    int spriteXFlip = (spriteTileAttributes >> 5) & 0x1;
-//                    int spriteYFlip = (spriteTileAttributes >> 6) & 0x1;
-//                    int spriteX = spriteData[1];
-//                    int spriteY = spriteData[0];
-//                    int spriteHeight = oam.getSpriteHeight(spriteIndexesOnLine.get(i));
-//
-//                    // adjust sprite coordinates for any flipping
-//                    if (oam.isSpriteFlippedHorizontally(spriteIndexesOnLine.get(i))) {
-//                        spriteX = 7 - spriteX;
-//                    }
-//                    if (oam.isSpriteFlippedVertically(spriteIndexesOnLine.get(i))) {
-//                        spriteY = spriteHeight - spriteY - 1;
-//                    }
-//
-//                    // read the appropriate tile data from VRAM
-//                    int spriteTileDataAddress = ((lcdc.getByte() & 0x4) == 0) ? 0x8000 : 0x8800;
-//                    int spriteTileDataIndex = memory.readByte(spriteTileDataAddress + (spriteTileIndex * 16) + ((line - spriteY) % spriteHeight) * 2);
-//                    int spriteTileDataAttributes = memory.readByte(spriteTileDataAddress + (spriteTileIndex * 16) + ((line - spriteY) % spriteHeight) * 2 + 1);
-//
-//                    // apply any necessary flipping
-//                    if (spriteXFlip == 1) {
-//                        spriteTileDataIndex = reverseBits(spriteTileDataIndex);
-//                    }
-//                    if (spriteYFlip == 1) {
-//                        spriteTileDataIndex = flipVertical(spriteTileDataIndex, spriteHeight);
-//                    }
-//
-//                    // get the color palette index for the current pixel
-//                    int spriteColorPaletteIndex = (spriteTileDataAttributes >> 3) & 0x7;
-//
-//                    // if the sprite pixel is not transparent, display it on the screen
-//                    if (spriteTileDataIndex != 0) {
-//                        // need to do something different on this line
-//                        int spritePaletteIndex = (spritePalette == 0) ? obp0.getByte() : obp1.getByte();
-//                        int spriteColorIndex = (spritePaletteIndex >> (spriteColorPaletteIndex * 2)) & 0x3;
-//                        int spriteColor = oam.getSpritePalette(spriteIndexesOnLine.get(i));
-//                       // display.setPixel(modeTicks, line, spriteColor);
-//                    }
-//
-//                    // update the display with the sprite data
-//                    if (spriteTileDataIndex != 0) {
-//                        // need to do something different on this line
-//                        int spritePaletteIndex = (spritePalette == 0) ? obp0.getByte() : obp1.getByte();
-//                        int spriteColorIndex = (spritePaletteIndex >> (spriteColorPaletteIndex * 2)) & 0x3;
-//                        int spriteColor = oam.getSpritePalette(spriteIndexesOnLine.get(i));
-//                        // update the screen buffer with the sprite info with setPixel method
-//                       // display.setPixel(modeTicks, line, spriteColor);
-//                    }
-//                } // sprite for
+
+                int sY;
+                int sX;
+                int sNum;
+                int sFlag;
+                List<Sprite> spriteList = Sprite.getAllSprites();
+                //System.out.println("spriteList size " +  spriteList.size());
+                // loop though visible sprites and create the spriteTiles to render them
+                if(!spriteList.isEmpty()) {
+                    //oam.printSpriteData();
+                    //System.out.println("Render sprite loop");
+                    for (int i = 0; i < spriteList.size(); i++) {
+                        // get sprite obj out of list
+                        Sprite sprite = spriteList.get(i);
+                        sY = sprite.getY() & 0xFF;
+                        sX = sprite.getX() & 0xFF;
+                        sNum = sprite.getTileNumber() & 0xFF;
+                        sFlag = sprite.getFlags() & 0xFF;
+                        Tile spriteTile = tileSet[sNum];
+                        for (int y = 0; y < 8; y++) {
+                            for (int x = 0; x < 8; x++) {
+                                int sPixel = spriteTile.getVal(y, x);
+                                int color = bgp.getColor(sPixel, 2);
+
+                                // calculate the pixel coordinates based on sprite position and current tile pixel position
+                                int xPosS = sX + x;
+                                int yPosS = sY + y;
+                                // write the pixel to the screen buffer
+                                //System.out.println(" setting pixel at " +xPosS+ ","+yPosS );
+                                display.setPixel(xPosS - 8, yPosS - 16, color);
+                                //display.setPixel(xPosS, yPosS, color);
+                            }
+                        }
+
+
+
+
+                    }
+                }
+
                 if (modeTicks >= 160&&curX>=160) {
                     // end of scanline
                     modeTicks = 0;
@@ -415,13 +375,13 @@ public class PPU {
         this.map = new TileMap(memory, address, ts);
     }
     public void printRAM() {//Proof of RAM working
-        for(int i=0;i<0x1000;i++) {//print ram hex values
+       /* for(int i=0;i<0x1000;i++) {//print ram hex values
             if(i%16==0)System.out.println();
             if((i&0xf)==0)System.out.print(" "+Integer.toHexString(0xc000+i));//maybe offset by location
             if(i%8==0)System.out.print(" | ");
             System.out.print(" "+Integer.toHexString(memory.readByte(0xc000+i))+" ");
          }
-        /*for(int y=0;y<8;y++) {//print a tile's values from set
+        for(int y=0;y<8;y++) {//print a tile's values from set
             for(int x=0;x<8;x++) {
                         //Can use map: map.getTile(X,Y) OR regular tile Index
                 System.out.print(" "+tileSet[map.getTile(0x08,0x02)].getVal(y,x)+" ");
@@ -432,7 +392,7 @@ public class PPU {
             boolean isStat3 = (stat.getBit(1)&&stat.getBit(0));
              if(y==7)System.out.println("-------------"+map.getTile(0x08,0x02)+" set:"+useTileSet0+" map:"+useBackgroundMap0
                 +" statMode3? "+isStat3);
-        }*
+        }
         System.out.println();
         for(int i=0;i<32;i++) {
             for (int j=0;j<32;j++) {
