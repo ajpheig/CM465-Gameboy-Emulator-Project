0482276d4fe6ce6eca93f697f9ac983bc1067a75
Alex Pheigaru <111469517+ajpheig@users.noreply.github.com>
2023-03-29 15:32:27 -0500

    Delete GBScreen.java

--- GBScreen.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-import javax.swing.*;
-
-public class GBScreen extends JPanel {
-    public GBScreen(int maxX, int maxY) {
-        setPreferredSize(new Dimension(maxX, maxY));
-        setBackground(Color.BLACK);
-    }
-}
